a8d1fc094eb1b4511ae8d098c6b32b26dc03f7f8
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-05-06 11:29:14 +0100

    " New Commit"

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f09e303e25mshb19946cb87c97eep189741jsn8389a089e942',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
